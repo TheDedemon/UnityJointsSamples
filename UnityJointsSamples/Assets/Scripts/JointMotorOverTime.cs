ec5ae2bb50cf6ce5de704ff97d5f015d23664534
Rémi Carreira <remi.carreira@muchobestostudio.fr>
2020-07-30 23:15:00 +0200

    [add] some joints samples

---
+++ UnityJointsSamples/Assets/Scripts/JointMotorOverTime.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace MuchoBestoStudio.JointsSystemSamples
+{
+	public class JointMotorOverTime : MonoBehaviour
+	{
+		#region Variables
+
+		[SerializeField]
+		private HingeJoint _hingeJoint = null;
+		[SerializeField]
+		private AnimationCurve _targetVelocityOverTime = null;
+		[SerializeField]
+		private AnimationCurve _forceOverTime = null;
+
+		private float _time = 0f;
+		private JointMotor _jointMotor;
+
+		#endregion
+
+		#region Awake
+
+		private void Awake()
+		{
+			_jointMotor = _hingeJoint.motor;
+		}
+
+		#endregion
+
+		#region Update
+
+		private void Update()
+		{
+			_time += Time.deltaTime;
+
+			_jointMotor.targetVelocity = _targetVelocityOverTime.Evaluate(_time);
+			_jointMotor.force = _forceOverTime.Evaluate(_time);
+
+			_hingeJoint.motor = _jointMotor;
+		}
+
+
+		#endregion
+	}
+}
